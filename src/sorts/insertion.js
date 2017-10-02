1a96d72c2e87716ef8dded7167eb4f39614e267b
Samuel Kwak <kwak123@gmail.com>
2017-10-02 11:12:57 -0400

    updated insertion and test suite to correctly stable sort

--- src/sorts/insertion.js
+++ src/sorts/insertion.js
@@ -6,12 +6,6 @@ var insertSort = function(object, filter = (a, b) => a < b ? -1 : a > b ?  1 : 0
         var replace = sortedObject.length;
         for (var i = 0; i < sortedObject.length; i++) {
             var result = filter(sortedObject[i], holder);
-            if (result === 0) {
-                if (sortedObject[i] > holder) {
-                    replace = i;
-                    break;
-                }
-            }
             if (result > 0) {
                 replace = i;
                 break;
